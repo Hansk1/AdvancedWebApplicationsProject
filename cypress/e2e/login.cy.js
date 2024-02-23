058f385240b468a1f4be16a7b40eca6e3a313494
Hansk1 <101644829+Hansk1@users.noreply.github.com>
2024-02-23 22:14:49 +0200

    Fixed login API tests

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -22,30 +22,60 @@ describe("User login API", () => {
             });
         });
     });
-});
 
-it("Login invalid credentials(password)", () => {
-    //Register the user:
-    const userData = {
-        firstName: "LoginTestUser2",
-        lastName: "LoginTestUser2",
-        email: "LoginTestUser2@example.com",
-        password: "LoginTestUser2222.",
-    };
-    cy.request("POST", "http://localhost:5000/users/register", userData).then(
-        (response) => {
+    it("Login invalid credentials(password)", () => {
+        //Register the user:
+        const userData = {
+            firstName: "LoginTestUser2",
+            lastName: "LoginTestUser2",
+            email: "LoginTestUser2@example.com",
+            password: "LoginTestUser2222.",
+        };
+        cy.request(
+            "POST",
+            "http://localhost:5000/users/register",
+            userData
+        ).then((response) => {
             expect(response.status).to.equal(200);
             //Test the login:
             cy.request("POST", "http://localhost:5000/users/login", {
                 email: userData.email,
-                password: "Invalid",
+                password: "Invalid1234.",
             }).then((loginResponse) => {
-                expect(loginResponse.status).to.equal(43);
+                expect(loginResponse.status).to.equal(403);
                 expect(response.body).to.have.property(
                     "message",
                     "Invalid credentials (Password)"
                 );
             });
-        }
-    );
+        });
+    });
+
+    it("Login invalid credentials (email)", () => {
+        //Register the user:
+        const userData = {
+            firstName: "LoginTestUser2",
+            lastName: "LoginTestUser2",
+            email: "LoginTestUser2@example.com",
+            password: "LoginTestUser2222.",
+        };
+        cy.request(
+            "POST",
+            "http://localhost:5000/users/register",
+            userData
+        ).then((response) => {
+            expect(response.status).to.equal(200);
+            //Test the login:
+            cy.request("POST", "http://localhost:5000/users/login", {
+                email: "invalid@example.com",
+                password: userData.password,
+            }).then((loginResponse) => {
+                expect(loginResponse.status).to.equal(403);
+                expect(response.body).to.have.property(
+                    "message",
+                    "Invalid credentials (Email)"
+                );
+            });
+        });
+    });
 });
