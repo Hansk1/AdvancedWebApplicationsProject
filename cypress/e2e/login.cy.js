3cd66c144f22787d8a48a52222cb2d909ef1e554
Hansk1 <101644829+Hansk1@users.noreply.github.com>
2024-02-23 21:43:26 +0200

    Added login API test number 1

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,25 @@
+describe("User registeration API", () => {
+    it("Login successfully", () => {
+        //Register the user:
+        const userData = {
+            firstName: "LoginTestUser1",
+            lastName: "LoginTestUser1",
+            email: "LoginTestUser1@example.com",
+            password: "LoginTestUser1111.",
+        };
+        cy.request(
+            "POST",
+            "http://localhost:5000/users/register",
+            userData
+        ).then((response) => {
+            expect(response.status).to.equal(200);
+            cy.request("POST", "http://localhost:5000/users/login", {
+                email: userData.email,
+                password: userData.password,
+            }).then((loginResponse) => {
+                expect(response.status).to.equal(200);
+                expect(response.body).to.have.property("succes", true);
+            });
+        });
+    });
+});
--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -40,27 +40,6 @@ describe("User registeration API", () => {
         });
     });
 
-    it("Register fails (Email already in use)", () => {
-        const userData = {
-            firstName: "User1",
-            lastName: "User1",
-            email: "user1@example.com",
-            password: "User1111.",
-        };
-        cy.request({
-            method: "POST",
-            url: "http://localhost:5000/users/register",
-            body: userData,
-            failOnStatusCode: false,
-        }).then((response) => {
-            expect(response.status).to.equal(403);
-            expect(response.body).to.have.property(
-                "email",
-                "Email already in use."
-            );
-        });
-    });
-
     it("Register fails (Invalid email format)", () => {
         const userData = {
             firstName: "User2",
@@ -83,6 +62,23 @@ describe("User registeration API", () => {
             firstName: "User3",
             lastName: "User3",
             email: "user3@example.com",
+            password: "invalid",
+        };
+        cy.request({
+            method: "POST",
+            url: "http://localhost:5000/users/register",
+            body: userData,
+            failOnStatusCode: false,
+        }).then((response) => {
+            expect(response.status).to.equal(400);
+        });
+    });
+
+    it("Register fails (Invalid password && Invalid email)", () => {
+        const userData = {
+            firstName: "User4",
+            lastName: "User4",
+            email: "invalid",
             password: "invalid",
         };
         cy.request({
